ec45f644777c8d294222ac19ddeda24497aac3db
Fynn Knoop <fknoop@techfak.de>
2020-07-18 14:49:43 +0200

    Added pie-chart for spinner odds

---
+++ src/components/SpinnerOddsChart.vue
@@ -0,0 +1,80 @@
+<script>
+    import { Pie } from 'vue-chartjs'
+    import 'chartjs-plugin-colorschemes';
+    import axios from "axios";
+
+    export default {
+        name: "SpinnerOddsChart",
+        props: ['category'],
+        data() {
+            return {
+                data: {
+                    labels: [],
+                    datasets: [{
+                        data: []
+                    }]
+                },
+                options: {
+                    plugins: {
+                        colorschemes: {
+                            scheme: 'brewer.SetThree12'
+                        }
+
+                    }
+                }
+            }
+        },
+        extends: Pie,
+        mounted() {
+            this.getData();
+        },
+        watch: {
+            category() {
+                this.getData();
+            }
+        },
+        methods: {
+            getData() {
+                this.data.labels = []
+                this.data.datasets[0].data = []
+                axios("https://api.epics.gg/api/v1/spinner", {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-user-jwt': this.$store.state.userdata.jwt
+                    },
+                    params: {
+                        'categoryId': this.category
+                    }
+                }).then(response => {
+                    if (response.data.success) {
+                        this.parseData(response.data.data.items)
+                    }
+                })
+            },
+            parseData(data) {
+                let unsorted = []
+                data.forEach(item => {
+                    unsorted.push({
+                        label: item.name,
+                        data: item.chance
+                    })
+                })
+                let sorted = unsorted.sort(function(a, b) {
+                    return b.data - a.data;
+                });
+
+                sorted.forEach(item => {
+                    this.data.labels.push(item.label)
+                    this.data.datasets[0].data.push(item.data)
+                })
+
+                this.renderChart(this.data, this.options)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,30 @@
 <template>
     <div>
         <Sidebar id="sidebar"></Sidebar>
-        <b-button v-b-toggle.sidebar style="float: left" class="ml-2">
-            <font-awesome-icon icon="angle-double-right"></font-awesome-icon>
-            Toggle Sidebar
+        <b-button v-b-toggle.sidebar style="float: left" class="ml-2 bg-transparent border-transparent">
+            <font-awesome-icon icon="align-justify" style="color: black"></font-awesome-icon>
         </b-button>
         <h1>Welcome {{this.$store.state.userdata.username}}!</h1>
+
+        <b-card-group columns class="ml-2 mr-2">
+            <b-card border-variant="dark" header="Spinner Odds" header-bg-variant="light">
+                <SpinnerOddsChart :category="$store.state.category"></SpinnerOddsChart>
+            </b-card>
+        </b-card-group>
+
     </div>
 </template>
 
 <script>
     import Sidebar from "../components/Sidebar";
+    import SpinnerOddsChart from "../components/SpinnerOddsChart";
+
     export default {
         name: "Home",
-        components: {Sidebar}
+        components: {SpinnerOddsChart, Sidebar},
+        created() {
+
+        }
     }
 </script>
 
