e4b3b3a7da7a667123c6715a952bbeb0143b9f50
Fynn Knoop <fknoop@techfak.de>
2020-07-19 13:10:02 +0200

    Added card sorting by template and images. Need to fix the grid layout somehow

--- src/pages/MassList.vue
+++ src/pages/MassList.vue
@@ -7,17 +7,49 @@
         <h1>Welcome {{this.$store.state.userdata.username}}!</h1>
 
         <b-row>
-            <b-col>
-                <b-card>
+            <b-col class="ml-2">
+                <b-card border-variant="dark" align="left">
                     <b-form-select v-model="season" :options="$store.state.seasons" @change="getCollections"/>
-                    <b-form-spinbutton/>
-                    <b-form-select
+                    <b-spinner class="mt-2" v-if="spinner.collections"/>
+                    <b-form-radio-group
+                            stacked
+                            class="mt-2"
                             v-model="collection"
                             :options="collectionOptions"
                             text-field="collection.name"
                             value-field="collection.id"
+                            @input="loadCards"
                     />
                 </b-card>
+            </b-col>
+            <b-col cols="8">
+                <b-card border-variant="dark">
+                    <b-spinner v-if="spinner.cardImages"/>
+                    <b-card-group deck v-else>
+                        <b-card v-for="card in cards"
+                                :key="card.templateId"
+                                style="max-height: 400px"
+                        >
+                            <b-row>
+                                <b-col>
+                                    <b-card-img :src="card.templateImage" @click="showMints(card)"/>
+                                </b-col>
+                                <b-col  v-show="card.showMints">
+                                    <b-list-group>
+                                        <b-list-group-item v-for="c in card.cards" :key="c.id">
+                                            {{c.mintBatch}}{{c.mintNumber}}
+                                        </b-list-group-item>
+                                    </b-list-group>
+                                </b-col>
+                            </b-row>
+                        </b-card>
+                    </b-card-group>
+
+
+                </b-card>
+            </b-col>
+            <b-col cols="2" class="mr-2">
+                <b-card border-variant="dark"></b-card>
             </b-col>
         </b-row>
     </div>
@@ -33,12 +65,19 @@
         data() {
             return {
                 season: null,
-                collection: null,
-                collectionOptions: []
+                collection: [],
+                collectionOptions: [],
+                cards: [],
+                spinner: {
+                    cardImages: false,
+                    collections: false
+                }
             }
         },
         methods: {
             getCollections() {
+                this.collectionOptions = []
+                this.spinner.collections = true
                 axios("https://api.epics.gg/api/v1/collections/users/32876/user-summary", {
                     method: 'GET',
                     headers: {
@@ -54,8 +93,61 @@
                         res.data.data.forEach(arr => {
                             this.collectionOptions = this.collectionOptions.concat(arr)
                         })
+                        this.spinner.collections = false
                     }
                 })
+            },
+            loadCards() {
+                this.cards = []
+                this.spinner.cardImages = true
+                axios(`https://api.epics.gg/api/v1/collections/${this.collection}/users/${this.$store.state.userdata.id}/owned2`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-user-jwt': this.$store.state.userdata.jwt
+                    },
+                    params: {
+                        'categoryId': this.$store.state.category
+                    }
+                }).then(res => {
+                    if (res.data.success) {
+                        res.data.data.cards.forEach(arr => {
+                            this.cards = this.cards.concat(arr)
+                        })
+                        this.sortByTemplate()
+                    }
+                })
+            },
+            sortByTemplate() {
+                let sorted = []
+                this.cards.forEach(card => {
+                    let template = sorted.find(s => {return s.templateId === card.cardTemplate.id})
+                    if (template) {
+                        template.cards.push({
+                            id: card.id,
+                            image: card.images.size402,
+                            mintBatch: card.mintBatch,
+                            mintNumber: card.mintNumber
+                        })
+                    } else {
+                        sorted.push({
+                            templateId: card.cardTemplate.id,
+                            templateImage: card.cardTemplate.images.size402,
+                            cards: [{
+                                id: card.id,
+                                image: card.images.size402,
+                                mintBatch: card.mintBatch,
+                                mintNumber: card.mintNumber
+                            }],
+                            showMints: false
+                        })
+                    }
+                })
+                this.cards = sorted
+                this.spinner.cardImages = false
+            },
+            showMints(card) {
+                card.showMints = !card.showMints
             }
         }
     }
