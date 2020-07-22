e917dba2291f6b92391d7a2f6fb2d1de33c771bb
Fynn Knoop <fknoop@techfak.uni-bielefeld.de>
2020-07-22 12:23:26 +0200

    Added masslisting for packs

--- src/pages/MassList.vue
+++ src/pages/MassList.vue
@@ -9,7 +9,13 @@
         <b-row>
             <b-col class="ml-2">
                 <b-card border-variant="dark" align="left">
-                    <b-form-select v-model="season" :options="$store.state.seasons" @change="getCollections"/>
+                    <b-form-select v-model="type" :options="typeOptions" @change="getPacks(1)"/>
+                    <b-form-select
+                            class="mt-2"
+                            v-if="type === 'Cards'"
+                            v-model="season"
+                            :options="$store.state.seasons"
+                            @change="getCollections"/>
                     <b-spinner class="mt-2" v-if="spinner.collections"/>
                     <b-form-radio-group
                             stacked
@@ -22,7 +28,20 @@
                     />
                 </b-card>
             </b-col>
-            <b-col cols="8">
+            <b-col cols="8" v-if="type === 'Packs'">
+                <b-card border-variant="dark">
+                    <b-row :align-h="spinner.cardImages ? 'center' : 'start'">
+                        <b-spinner v-if="spinner.cardImages"/>
+                        <b-col cols="3" v-else v-for="pack in packs" :key="pack.templateId" class="mb-3" @click="selectPack(pack)">
+                            <b-card>
+                                <b-badge>x{{pack.count}}</b-badge>
+                                <b-card-img :src="pack.templateImage"/>
+                            </b-card>
+                        </b-col>
+                    </b-row>
+                </b-card>
+            </b-col>
+            <b-col cols="8" v-else>
                 <b-card border-variant="dark">
                     <b-row :align-h="spinner.cardImages ? 'center' : 'start'">
                         <b-spinner v-if="spinner.cardImages"/>
@@ -46,7 +65,30 @@
                     </b-row>
                 </b-card>
             </b-col>
-            <b-col cols="2" class="mr-2">
+            <b-col v-if="type === 'Packs'" cols="2" class="mr-2">
+                <b-card border-variant="dark">
+                    <b-card v-for="pack in selected" :key="pack.templateId">
+                        <span class="font-weight-bold">
+                            {{pack.name}}
+                        </span>
+                        <b-form-group label="Amount: ">
+                            <b-form-input v-model="pack.sellCount" number></b-form-input>
+                        </b-form-group>
+                        <b-form-group label="Price: ">
+                            <b-form-input v-model="pack.price" number></b-form-input>
+                        </b-form-group>
+
+                    </b-card>
+                </b-card>
+                <b-card v-if="selected.length > 0">
+                    <b-form-radio-group v-model="packSellType" :options="packSellOptions"/>
+                    <b-button variant="primary" @click="sellPacks">
+                        <b-spinner v-if="spinner.selling"/>
+                        <span v-else>Sell</span>
+                    </b-button>
+                </b-card>
+            </b-col>
+            <b-col v-else cols="2" class="mr-2">
                 <b-card border-variant="dark">
                     <b-card v-for="card in selected" :key="card.id">
                         <p class="font-weight-bold">{{`${card.mintBatch}${card.mintNumber}`}}</p>
@@ -70,17 +112,85 @@
         data() {
             return {
                 season: null,
+                type: null,
+                typeOptions: [
+                    'Cards',
+                    'Packs'
+                ],
                 collection: [],
                 collectionOptions: [],
                 cards: [],
+                packs: [],
                 selected: [],
                 spinner: {
                     cardImages: false,
-                    collections: false
-                }
+                    collections: false,
+                    selling: false
+                },
+                packSellOptions: ['newest', 'oldest'],
+                packSellType: ['newest']
             }
         },
         methods: {
+            getPacks(page) {
+                this.spinner.cardImages = true
+                if (this.type === 'Packs') {
+                    axios("https://api.epics.gg/api/v1/packs/user", {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'x-user-jwt': this.$store.state.userdata.jwt
+                        },
+                        params: {
+                            'categoryId': this.category,
+                            page: page
+                        }
+                    }).then(response => {
+                        if (response.data.success) {
+                            if (response.data.data.count > 0) {
+                                this.packs = this.packs.concat(response.data.data.packs)
+                                this.getPacks(++page)
+                            } else {
+                                this.sortPacks();
+                            }
+                        }
+                    })
+                }
+            },
+            sortPacks() {
+                let sorted = []
+                this.packs.forEach(pack => {
+                    let template = sorted.find(s => {return s.templateId === pack.packTemplate.id})
+                    if (template) {
+                        template.packs.push({
+                            id: pack.id,
+                            created: new Date(pack.created)
+                        })
+                        template.count++
+                    } else {
+                        sorted.push({
+                            disabled: false,
+                            templateId: pack.packTemplate.id,
+                            templateImage: `http://cdn.epics.gg${pack.packTemplate.images[0].url}`,
+                            name: pack.packTemplate.name,
+                            count: 1,
+                            sellCount: 0,
+                            price: 1,
+                            packs: [{
+                                id: pack.id,
+                                created: new Date(pack.created)
+                            }],
+                        })
+                    }
+                })
+                this.packs = sorted
+                this.packs.forEach(template => {
+                    template.packs.sort((a,b) => {
+                        return b.created - a.created    //oldest first
+                    })
+                })
+                this.spinner.cardImages = false
+            },
             getCollections() {
                 this.collectionOptions = []
                 this.spinner.collections = true
@@ -156,6 +266,53 @@
             },
             showMints(card) {
                 card.showMints = !card.showMints
+            },
+            selectPack(pack) {
+                if (!pack.disabled) {
+                    this.selected.push(pack)
+                    pack.disabled = true
+                }
+            },
+            sellPacks() {
+                this.spinner.selling = true
+                if (this.packSellType === 'newest') {
+                    this.selected.forEach(template => {
+                        for (let i = 0; i <= template.sellCount; i++) {
+                            let pack = template.packs[i]
+                            this.sellItem(pack.id, 'pack', template.price, null)
+                        }
+                    })
+                } else {
+                    this.selected.forEach(template => {
+                        for (let i = template.count - 1; i >= (template.count - template.sellCount); i--) {
+                            let pack = template.packs[i]
+                            this.sellItem(pack.id, 'pack', template.price, null)
+                        }
+                    })
+                }
+                this.spinner.selling = false
+            },
+            sellItem(id, type, price, minOffer) {
+                axios("https://api.epics.gg/api/v1/market/list", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-user-jwt': this.$store.state.userdata.jwt
+                    },
+                    params: {
+                        'categoryId': this.$store.state.category
+                    },
+                    data: {
+                        'id': id,
+                        'minOffer': minOffer,
+                        'price': price,
+                        'type': type
+                    }
+                }).then(res => {
+                    if (res.data.success) {
+                        console.log(res)
+                    }
+                })
             }
         }
     }
