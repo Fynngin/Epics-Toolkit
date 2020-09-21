ffc839d543e942769bef0379800e4142a3099c4a
Fynn Knoop <fknoop@techfak.uni-bielefeld.de>
2020-09-21 12:04:26 +0200

    Finished account transfer. Needs battle testing tho

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {faEdit, faUserPlus, faSave, faMagic, faAlignJustify, faEllipsisV, faCogs, faExchangeAlt, faLink, faTrashAlt,
   faPlusCircle, faCaretRight, faCaretDown, faRandom, faArrowUp, faArrowDown, faWindowClose, faChartLine, faTimesCircle,
-  faHistory, faSearch} from '@fortawesome/free-solid-svg-icons'
+  faHistory, faSearch, faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons'
 import VueFlags from '@growthbunker/vueflags';
 
 library.add(faUserPlus, faEdit, faSave, faMagic, faAlignJustify, faEllipsisV, faCogs, faExchangeAlt, faLink, faTrashAlt,
     faPlusCircle, faCaretRight, faCaretDown, faRandom,faArrowUp, faArrowDown, faWindowClose, faChartLine, faTimesCircle,
-    faHistory, faSearch)
+    faHistory, faSearch, faArrowLeft, faArrowRight)
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
--- src/pages/AccountTransfer.vue
+++ src/pages/AccountTransfer.vue
@@ -1,37 +1,70 @@
 <template>
     <div>
         <Sidebar id="sidebar"></Sidebar>
-        <b-button v-b-toggle.sidebar style="float: left" class="ml-2 bg-transparent border-transparent">
-            <font-awesome-icon icon="align-justify" style="color: black"></font-awesome-icon>
-        </b-button>
-
-        <b-row>
-            <b-col cols="6">
-                <h1>Send trades</h1>
-                <p>Coming soon...</p>
-                <b-button @click="createTradeList">TEST</b-button>
+        <b-row class="mb-2">
+            <b-button v-b-toggle.sidebar style="float: left" class="ml-4 bg-transparent border-transparent">
+                <font-awesome-icon icon="align-justify" style="color: black"></font-awesome-icon>
+            </b-button>
+        </b-row>
+        <b-row align-h="center" class="mb-2">
+            <b-col cols="10">
+                <v-stepper ref="stepper" :steps="steps" v-model="step"/>
             </b-col>
-            <b-col cols="6">
-                <h1>Accept trades</h1>
+        </b-row>
+        <b-button
+            variant="outline-dark"
+            class="mb-4 button"
+            @click="$refs.stepper.previous()"
+        ><font-awesome-icon icon="arrow-left"/><br>Previous</b-button>
+        <b-button
+            variant="outline-dark"
+            class="ml-2 mb-4 button"
+            @click="$refs.stepper.next()"
+            :disabled="checkStepperRequirements"
+        ><font-awesome-icon icon="arrow-right"/><br>Next</b-button>
 
+        <div v-if="step === 1">
+            <h2>Do you want to send or accept trades?</h2>
+            <h5>Sending will send the items from a given account to your account.</h5>
+            <h5>Accepting will accept all incoming trades from a given account</h5>
+            <b-button
+                @click="action = 'send'"
+                :variant="action === 'send' ? 'success' : 'outline-success'"
+                class="button"
+            >Send</b-button>
+            <b-button
+                @click="action = 'accept'"
+                :variant="action === 'accept' ? 'success' : 'outline-success'"
+                class="ml-2 button"
+            >Accept</b-button>
+        </div>
+
+        <div v-if="step === 2">
+            <h2 v-if="action === 'send'">What account do you want to transfer the items from?</h2>
+            <h2 v-else>What account do you want to accept the trades from?</h2>
+
+            <p v-if="selectedUser">Selected user: <strong>{{this.selectedUser.username}}</strong></p>
+
+            <b-input-group prepend="Search users:" class="ml-2" v-on:submit.prevent="searchUsers">
+                <b-form-input v-model="userSearch" v-on:submit.prevent="searchUsers"></b-form-input>
+                <b-input-group-append>
+                    <b-button size="sm" variant="outline-dark" @click="searchUsers">Search</b-button>
+                </b-input-group-append>
+            </b-input-group>
+
+            <b-list-group class="mt-2 ml-2" v-if="showUserList">
+                <b-list-group-item v-for="(user, index) in userOptions" :key="index" @click="selectUser(user)" href="#">
+                    <b-avatar :src="`${$store.state.cdnUrl}${user.avatar}`"/>
+                    {{user.username}}
+                </b-list-group-item>
+            </b-list-group>
+        </div>
+
+
+        <template v-if="step === 3">
+            <div v-if="action === 'accept'">
                 <p v-if="selectedUser">There are <strong>{{filteredTrades.length}}</strong> trades from <strong>{{selectedUser.username}}</strong> to accept.</p>
-
-                <b-input-group prepend="Search users:">
-                    <b-form-input v-model="userSearch"></b-form-input>
-                    <b-input-group-append>
-                        <b-button size="sm" variant="outline-dark" @click="searchUsers">Search</b-button>
-                    </b-input-group-append>
-                </b-input-group>
-
-                <b-list-group class="mt-2" style="overflow: scroll; max-height: 200px;" v-if="showUserList">
-                    <b-list-group-item v-for="(user, index) in userOptions" :key="index" @click="selectUser(user)" href="#">
-                        <b-avatar :src="`${$store.state.cdnUrl}${user.avatar}`"/>
-                        {{user.username}}
-                    </b-list-group-item>
-                </b-list-group>
-
-                <b-button v-if="selectedUser && filteredTrades.length > 0" class="mt-2" variant="outline-primary" @click="acceptTrades">Accept trades</b-button>
-
+                <b-button class="button" variant="outline-success" v-if="filteredTrades.length > 0" @click="acceptTrades">Accept</b-button>
                 <b-spinner v-if="spinner.accepting"/>
                 <b-progress v-if="showAcceptProgress" class="mt-2" :max="filteredTrades.length">
                     <b-progress-bar :value="acceptProgress">
@@ -39,20 +72,85 @@
                     </b-progress-bar>
                 </b-progress>
                 <p v-if="!spinner.accepting && acceptProgress > 0">Done! <strong v-if="amountFailedAccepts > 0">Failed to accept {{amountFailedAccepts}} trades!</strong></p>
-            </b-col>
-        </b-row>
+            </div>
+
+            <div v-else>
+                <p>Click "Start" to begin the process. The program will start sorting {{selectedUser.username}}'s cards.</p>
+                <p>After that, you have to confirm the process one more time.</p>
+                <b-button v-if="!progress.seasons" @click="createTradeList" variant="outline-success">Prepare trades</b-button>
+                <p v-if="progress.seasons && progress.seasons < $store.state.seasons.length">
+                    Getting seasons and collections...
+                </p>
+                <p v-if="progress.collections.max && progress.collections.progress < progress.collections.max">
+                    Getting items...
+                </p>
+                <p v-if="progress.grouping.max && progress.grouping.progress < progress.grouping.max">
+                    Grouping items...
+                </p>
+                <p v-if="progress.grouping.progress === progress.grouping.max && !progress.sending">
+                    Done! You can now start the sending process. The program will send <strong>{{idList.length}}</strong> trades in total.
+                </p>
+                <p v-if="progress.sending && progress.sending < idList.length">
+                    Sending trades...
+                </p>
+                <p v-if="progress.sending === idList.length">
+                    All done! You can now log in with the other account and start accepting the trades.
+                </p>
+                <b-progress
+                    show-progress
+                    class="mb-2 ml-2"
+                    v-if="progress.seasons"
+                    :value="progress.seasons"
+                    :max="$store.state.seasons.length"
+                    :animated="progress.seasons < $store.state.seasons.length"
+                    :variant="progress.seasons < $store.state.seasons.length ? 'primary' : 'success'"
+                />
+                <b-progress
+                    show-progress
+                    class="mb-2 ml-2"
+                    v-if="progress.collections.max"
+                    :value="progress.collections.progress"
+                    :max="progress.collections.max"
+                    :animated="progress.collections.progress < progress.collections.max"
+                    :variant="progress.collections.progress < progress.collections.max ? 'primary' : 'success'"
+                />
+                <b-progress
+                    show-progress
+                    class="mb-2 ml-2"
+                    v-if="progress.grouping.max"
+                    :value="progress.grouping.progress"
+                    :max="progress.grouping.progress"
+                    :animated="progress.grouping.progress < progress.grouping.max"
+                    :variant="progress.grouping.progress < progress.grouping.max ? 'primary' : 'success'"
+                />
+                <b-progress
+                    show-progress
+                    class="mb-2 ml-2"
+                    v-if="progress.sending"
+                    :value="progress.sending"
+                    :max="idList.length"
+                    :animated="progress.sending < idList.length"
+                    :variant="progress.sending < idList.length ? 'primary' : 'success'"
+                />
+                <b-button v-if="progress.grouping.progress === progress.grouping.max && !progress.sending" @click="sendTrades" variant="outline-success">Send trades</b-button>
+            </div>
+        </template>
     </div>
 </template>
 
 <script>
 import Sidebar from "@/components/Sidebar";
+import { VStepper } from 'vue-stepper-component'
 import {acceptTrade, getCardIds, getCollections, getStickerIds, getTrades, searchUsers, sendTrade} from "@/api";
 
 export default {
     name: "AccountTransfer",
-    components: {Sidebar},
+    components: {Sidebar, VStepper},
     data() {
         return {
+            steps: 3,
+            step: 1,
+            action: null,
             acceptProgress: 0,
             amountFailedAccepts: 0,
             userSearch: '',
@@ -65,8 +163,19 @@ export default {
             spinner: {
                 accepting: false
             },
-            purples: [],
-            tradeProgress: 0
+            progress: {
+                seasons: null,
+                collections: {
+                    progress: 0,
+                    max: null
+                },
+                grouping: {
+                    progress: 0,
+                    max: null
+                },
+                sending: null
+            },
+            purples: []
         }
     },
     async created() {
@@ -91,6 +200,16 @@ export default {
                 })
             }
             return []
+        },
+        checkStepperRequirements() {
+            switch (this.step) {
+                case 1:
+                    return this.action === null
+                case 2:
+                    return this.selectedUser === null
+                default:
+                    return true
+            }
         }
     },
     methods: {
@@ -121,10 +240,10 @@ export default {
             this.spinner.accepting = false
         },
         async createTradeList() {
+            this.progress.seasons = 0
             let collections = []
             let cardids = []
             let stickerids = []
-            let progress = 0
 
             for (const season of this.$store.state.seasons) {
                 await getCollections(this.$store.state.userdata.jwt, this.$store.state.category, season, this.selectedUser.id).then(res => {
@@ -132,9 +251,12 @@ export default {
                         collections = collections.concat(res.data.data.filter(coll => {
                             return coll.count > 0
                         }))
+                        this.progress.seasons++
                     }
                 })
             }
+
+            this.progress.collections.max = collections.length
             for (const coll of collections) {
                 if (coll.collection.properties['entity_types'].includes('card')) {
                     await getCardIds(this.$store.state.userdata.jwt, this.$store.state.category, coll.collection.id, this.selectedUser.id).then(res => {
@@ -154,9 +276,11 @@ export default {
                         }
                     })
                 }
-                console.log(`${++progress} / ${collections.length}`)
+                this.progress.collections.progress++
             }
 
+            this.progress.grouping.progress = 0
+            this.progress.grouping.max = Math.ceil(cardids.length / 49) + Math.ceil(stickerids.length / 49)
             let startIdx = 0
             let endIdx = 49
             for (let i = 0; i < (cardids.length / 49); i++) {
@@ -164,6 +288,7 @@ export default {
                 this.idList.push(temp.map(elem => {
                     return {id: elem, type: 'card'}
                 }))
+                this.progress.grouping.progress++
                 startIdx += 49
                 endIdx += 49
             }
@@ -174,6 +299,7 @@ export default {
                 this.idList.push(temp.map(elem => {
                     return {id: elem, type: 'sticker'}
                 }))
+                this.progress.grouping.progress++
                 startIdx += 49
                 endIdx += 49
             }
@@ -186,20 +312,18 @@ export default {
                     })
                 }
             })
-
-            await this.sendTrades().then(() => console.log('Done!'))
         },
         async sendTrades() {
-            this.tradeProgress = 0
+            this.progress.sending = 0
             for (const batch of this.idList) {
                 let entities = batch.concat([{id: this.purples[0]['cardIds'][0], type: 'card'}])
                 this.purples[0]['cardIds'].splice(0, 1)
                 await sendTrade(this.$store.state.userdata.jwt, this.$store.state.category, this.selectedUser.id, entities).then(res => {
                     if (res.data.success) {
-                        console.log(`${++this.tradeProgress} / ${this.idList.length}`)
                         this.purples = this.purples.sort((a,b) => {
                             return b['cardIds'].length - a['cardIds'].length
                         })
+                        this.progress.sending++
                     }
                 })
             }
@@ -209,5 +333,7 @@ export default {
 </script>
 
 <style scoped>
-
+    .button {
+        width: 100px;
+    }
 </style>
